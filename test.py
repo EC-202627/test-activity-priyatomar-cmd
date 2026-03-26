3d5254a078b3adf12de25e2313020faf4ead222f
priyatomar-cmd <priya.tomar@avantika.edu.in>
2026-03-26 07:58:03 +0000

    Too much exected

--- test.py
+++ test.py
@@ -8,7 +8,9 @@
 T=int(input())
 for i in range(T):
     x=int(input())
-    if x>=2000:
+    if x>=2000 and x<10000:
         print("Yes")
+    elif x>=10000:
+        print("It's too much")    
     else:
         print("No")
